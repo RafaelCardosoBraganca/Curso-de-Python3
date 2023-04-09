ad272e17d9ba8bb5deaf39754df5c3197eb8d13b
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-09 15:17:21 -0300

    updates

--- desafio2_aula6.py
+++ desafio2_aula6.py
@@ -1,10 +1,13 @@
 n=input('Escreva algo: ')
+print(type(n))
 print('é alfa numérico?',n.isalnum())
 print('é numérico?',n.isnumeric())
-print('é alfabetico?',n.isalpha())
+print('é alfabético?',n.isalpha())
 print('é todo minúsculo?',n.islower())
 print('é todo maiúsculo?',n.isupper())
-print('é decima?',n.isdecimal())
+print('é capitalizado?',n.isascii())
+
+
 
 
 
