9c20aedf9f4fa4ee6724287766fbdebe086d929a
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-09 18:01:51 -0300

    Update aula7_desafio9.py

--- aula7_desafio9.py
+++ aula7_desafio9.py
@@ -1,4 +1,4 @@
 #crie um script que leia um salário e informe qual será seu valor com 15% de aumento
 salario=float(input('Informe seu salário: '))
 aumento= salario*0.15+salario
-print('Com 15 por cento de aumento seu novo salário será R${}!'.format(aumento))
+print('Com 15 por cento de aumento seu novo salário será R${:.2f}!'.format(aumento))
