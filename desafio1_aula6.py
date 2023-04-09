9710297a8767f0f4e4e415ad1abeb6114a700e3a
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-09 00:29:39 -0300

    updates curso python

---
+++ desafio1_aula6.py
@@ -0,0 +1,6 @@
+n1=int(input('Digite o primeiro número '))
+n2=int(input('Digite o segundo número '))
+soma=n1+n2
+# print('a soma entre',n1,'e',n2,'vale',soma) como eu fiz kkkk
+print('a soma entre {} e {} vale {}'.format(n1, n2, soma)) #como o Guanabara faria!!
+
---
+++ desafio2_aula6.py
@@ -0,0 +1,10 @@
+n=input('Escreva algo: ')
+print('é alfa numérico?',n.isalnum())
+print('é numérico?',n.isnumeric())
+print('é alfabetico?',n.isalpha())
+print('é todo minúsculo?',n.islower())
+print('é todo maiúsculo?',n.isupper())
+print('é decima?',n.isdecimal())
+
+
+
