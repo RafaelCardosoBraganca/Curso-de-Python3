f69f44c3c0f4270780ab78a7ebab2e4786c37af3
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-09 16:41:50 -0300

    update

---
+++ aula7_desafio4.py
@@ -0,0 +1,5 @@
+#ler uma medida em metros e informar seu valor em centímetros e milímetros
+m=float(input('Informe algum valor em metros: '))
+cm= m*100
+mm= m*1000
+print('São {} metros que equivalem à {} centímetros e à {} milímetros'.format(m, cm, mm))
