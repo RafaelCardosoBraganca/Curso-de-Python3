a9ba3e5cf9cd5115a8961bb6e5b934ffe232c9c3
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-09 16:09:33 -0300

    update

---
+++ aula7_desafio2.py
@@ -0,0 +1,6 @@
+#algoritmo que leia um número, mostre seu dobro, triplo e raiz quadrada
+n=float(input('insira um número: '))
+d= n*2
+t= n*3
+r= n**(1/2)
+print('o dobro de {}  é {}, seu triplo é {} e sua raiz quadrada é {}'.format(n, d, t, r))
