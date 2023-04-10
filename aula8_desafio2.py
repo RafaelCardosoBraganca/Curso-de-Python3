f9fdaae205697abe32e591873a6b3c3afa0747e8
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-10 02:10:42 -0300

    Cálculo de hipotenusa

---
+++ aula8_desafio2.py
@@ -0,0 +1,14 @@
+#faça um script que calcule os catetos e hipotenuso de um triângulo 
+#usando o teorema de pitágoras e os módulos Math, lendo os comprimentos dos 2 catetos
+
+from math import sqrt
+print('Calcule a sua hipotenusa!!!')
+print('-'*20)
+co= int(input('Informe o comprimento do Cateto Oposto: '))
+ca= int(input('Informe o comprimento do Cateto Adjacente: '))
+#hp= pow(ca, 2) + pow(co, 2)
+hp= sqrt(pow(ca, 2) + pow(co, 2))
+print('-'*20)
+print('O comprimento da Hipotenusa é {} !!!'.format(hp))
+
+#print('o Comprimento da Hipotenusa é {:.1f} !!!!'.format(sqrt(hp)))
