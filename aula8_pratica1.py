39403d20b26f9d44abf6295ac18b996520367574
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-09 22:50:11 -0300

    update

---
+++ aula8_pratica1.py
@@ -0,0 +1,6 @@
+#aula sobre módulos, importando o módulo inteiro
+
+import math
+num=int(input('digite um número: '))
+raiz= math.sqrt(num)
+print('a raiz de {} é {}'.format(num, raiz))
---
+++ aula8_pratica2.py
@@ -0,0 +1,5 @@
+#aula sobre módulos, importando parte do módulo
+from math import sqrt, floor
+num=int(input('digite um número: '))
+raiz= sqrt(num)
+print('a raiz de {} é {}'.format(num, floor(raiz)))
